b79fd4e2ce829db67361841a4abfe275cf73eef5
Austin Schuster <austinschusters@gmail.com>
2022-05-01 02:42:49 -0400

    Functioning multiselect filter for bar graph
    Bar graph works with filters. Working on more complex filters.

---
+++ TestingSearch.py
@@ -0,0 +1,42 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Output, Input, State
+import pandas as pd
+import plotly.express as px
+app = Dash(__name__)
+
+df = pd.read_csv('Cleaned_Laptop_data.csv')
+
+group_df = df.groupby(['brand'])['brand'].count().reset_index(name = 'total')\
+    .sort_values(['total'], ascending = False)
+
+fig = px.scatter(x=group_df['brand'], y=group_df['total'])
+
+app.layout = html.Div([
+    dcc.Dropdown(id='brand_selector',
+                 options=[{'label': i, 'value': i} for i in df['brand'].unique()],
+                 value=group_df['brand'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select Brands To Filter By'),
+    dcc.Graph(id='graph',
+        figure={}
+        )
+    ])
+@app.callback(
+    Output(component_id='graph', component_property='figure'),
+    [Input(component_id='brand_selector', component_property='value')]
+)
+def update_graph(brand_selected):
+    if len(brand_selected) > 0:
+        dff = group_df[group_df['brand'].isin(brand_selected)]
+        fig = px.bar(group_df, x=dff['brand'], y=dff['total'])
+
+        return fig
+    else:
+        fig = px.bar(x=group_df['brand'], y=group_df['total'])
+        return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- bubbletest.py
+++ bubbletest.py
@@ -8,7 +8,7 @@ df = pd.read_csv('../Final-Project-ITSC-3155/Cleaned_Laptop_data.csv')
 data = [
     go.Scatter(x=df['star_rating'],
                y=df['latest_price'],
-               text=df['brand'],
+               text=df['model'],
                mode='markers',
                marker=dict(size=df['latest_price']/5000,color=df['star_rating'], showscale=True)
                )
