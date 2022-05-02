67538b05d5cdcd965756627e7a9478ad2d252936
Austin Schuster <austinschusters@gmail.com>
2022-05-02 18:18:06 -0400

    fixed filter

--- ScatterMultiSelect.py
+++ ScatterMultiSelect.py
@@ -8,7 +8,7 @@ app = Dash(__name__)
 df = pd.read_csv('Cleaned_Laptop_data.csv')
 
 fig = px.scatter(df, x='star_rating', y='latest_price', color='star_rating', size='latest_price',
-                 hover_data=['brand', 'model'])
+                 hover_data=['brand', 'model', 'processor_brand'])
 
 app.layout = html.Div([
     dcc.Dropdown(id='brand_selector',
@@ -27,6 +27,15 @@ app.layout = html.Div([
                  searchable=True,
                  clearable=True,
                  placeholder='Select Models To Filter By'),
+    dcc.Dropdown(id='processor_brand_selector',
+                 options=[{'label': i, 'value': i} for i in df['processor_brand'].unique()]
+                         + [{'label': 'Select All', 'value': 'allProcessorBrand'}],
+                 value=df['processor_brand'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select Processor Brand To Filter By'),
+
     dcc.Graph(id='graph',
               figure={}
               )
@@ -36,31 +45,41 @@ app.layout = html.Div([
 @app.callback(
     Output(component_id='graph', component_property='figure'),
     [Input(component_id='brand_selector', component_property='value')],
-    [Input(component_id='model_selector', component_property='value')]
+    [Input(component_id='model_selector', component_property='value')],
+    [Input(component_id='processor_brand_selector', component_property='value')],
 
 )
-def update_graph(brand_selected, model_selector):
-    if (len(brand_selected) > 0) | (len(model_selector) > 0):
-        if 'allBrand' in brand_selected:
-            dff = df
-        elif (len(brand_selected) < 1):
-            dff=df
+def update_graph(brand_selector, model_selector, processor_brand_selector):
+    if (len(brand_selector) > 0) | (len(model_selector) > 0 | len(processor_brand_selector) > 0):
+        dff = df
+        if 'allBrand' in brand_selector:
+            dff = dff
+        elif (len(brand_selector) < 1):
+            dff = dff
         else:
-            dff = df[df['brand'].isin(brand_selected)]
+            dff = dff[dff['brand'].isin(brand_selector)]
+
         if 'allModel' in model_selector:
             dff = dff
         elif (len(model_selector) < 1):
-            dff=df
+            dff = dff
         else:
             dff = dff[dff['model'].isin(model_selector)]
 
+        if 'allProcessorBrand' in processor_brand_selector:
+            dff = dff
+        elif (len(processor_brand_selector) < 1):
+            dff = dff
+        else:
+            dff = dff[dff['processor_brand'].isin(processor_brand_selector)]
+
         fig = px.scatter(dff, x='star_rating', y='latest_price', color='star_rating', size='latest_price',
-                         hover_data=['brand', 'model'])
+                         hover_data=['brand', 'model', 'processor_brand'])
 
         return fig
     else:
         fig = px.scatter(df, x='star_rating', y='latest_price', color='star_rating', size='latest_price',
-                         hover_data=['brand', 'model'])
+                         hover_data=['brand', 'model', 'processor_brand'])
         return fig
 
 
