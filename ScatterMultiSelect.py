c619ab53b658ba4899c9dfa6fa8ee144fbe3f637
Jack <jdwoodworth7@gmail.com>
2022-05-02 17:19:21 -0400

    jack w adding hdd os weight display size touch screen to ScatterMultiSelect.py

--- ScatterMultiSelect.py
+++ ScatterMultiSelect.py
@@ -8,7 +8,7 @@ app = Dash(__name__)
 df = pd.read_csv('Cleaned_Laptop_data.csv')
 
 fig = px.scatter(df, x='star_rating', y='latest_price', color='star_rating', size='latest_price',
-                 hover_data=['brand', 'model', 'ram_gb', 'processor_brand', 'processor_name', 'ram_type', 'ssd'])
+                 hover_data=['brand', 'model', 'ram_gb', 'processor_brand', 'processor_name', 'ram_type', 'ssd', 'hdd', 'os', 'weight', 'display_size', 'Touchscreen'])
 
 app.layout = html.Div([
     dcc.Dropdown(id='brand_selector',
@@ -19,6 +19,7 @@ app.layout = html.Div([
                  searchable=True,
                  clearable=True,
                  placeholder='Select Brands To Filter By'),
+
     dcc.Dropdown(id='model_selector',
                  options=[{'label': i, 'value': i} for i in df['model'].unique()]
                          + [{'label': 'Select All', 'value': 'allModel'}],
@@ -27,6 +28,7 @@ app.layout = html.Div([
                  searchable=True,
                  clearable=True,
                  placeholder='Select Models To Filter By'),
+
     dcc.Dropdown(id='processor_brand_selector',
                  options=[{'label': i, 'value': i} for i in df['processor_brand'].unique()]
                          + [{'label': 'Select All', 'value': 'allProcessorBrand'}],
@@ -70,7 +72,52 @@ app.layout = html.Div([
                  multi=True,
                  searchable=True,
                  clearable=True,
-                 placeholder='Select SSD To Filter By'),
+                 placeholder='Select SSD in GB To Filter By'),
+
+    dcc.Dropdown(id='hdd_selector',
+                 options=[{'label': i, 'value': i} for i in df['hdd'].unique()]
+                         + [{'label': 'Select All', 'value': 'allHDD'}],
+                 value=df['hdd'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select HDD in GB To Filter By'),
+
+    dcc.Dropdown(id='os_selector',
+                 options=[{'label': i, 'value': i} for i in df['os'].unique()]
+                         + [{'label': 'Select All', 'value': 'allOS'}],
+                 value=df['os'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select Operating Systems To Filter By'),
+
+    dcc.Dropdown(id='weight_selector',
+                 options=[{'label': i, 'value': i} for i in df['weight'].unique()]
+                         + [{'label': 'Select All', 'value': 'allWeight'}],
+                 value=df['weight'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select Laptop Weight To Filter By'),
+
+    dcc.Dropdown(id='display_size_selector',
+                 options=[{'label': i, 'value': i} for i in df['display_size'].unique()]
+                         + [{'label': 'Select All', 'value': 'allDisplaySize'}],
+                 value=df['display_size'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select Display Size To Filter By'),
+
+    dcc.Dropdown(id='touchscreen_selector',
+                 options=[{'label': i, 'value': i} for i in df['Touchscreen'].unique()]
+                         + [{'label': 'Select All', 'value': 'allTouchscreen'}],
+                 value=df['Touchscreen'].unique(),
+                 multi=True,
+                 searchable=True,
+                 clearable=True,
+                 placeholder='Select if Touchscreen To Filter By'),
 
     dcc.Graph(id='graph',
               figure={}
@@ -87,6 +134,11 @@ app.layout = html.Div([
     [Input(component_id='ram_selector', component_property='value')],
     [Input(component_id='ram_type_selector', component_property='value')],
     [Input(component_id='ssd_selector', component_property='value')]
+    [Input(component_id='hdd_selector', component_property='value')]
+    [Input(component_id='os_selector', component_property='value')]
+    [Input(component_id='weight_selector', component_property='value')]
+    [Input(component_id='display_size_selector', component_property='value')]
+    [Input(component_id='touchscreen_selector', component_property='value')]
 
 )
 def update_graph(brand_selected, model_selector, ram_selector, processor_brand_selector, processor_name_selector,
@@ -141,17 +193,53 @@ def update_graph(brand_selected, model_selector, ram_selector, processor_brand_s
         elif len(ssd_selector) < 1:
             dff = df
         else:
-            dff = df[df['brand'].isin(ssd_selector)]
+            dff = df[df['ssd'].isin(ssd_selector)]
+
+        if 'allHDD' in hdd_selector:
+            dff = df
+        elif len(hdd_selector) < 1:
+            dff = df
+        else:
+            dff = df[df['hdd'].isin(hdd_selector)]
+
+        if 'allOS' in os_selector:
+            dff = df
+        elif len(os_selector) < 1:
+            dff = df
+        else:
+            dff = df[df['os'].isin(os_selector)]
+
+        if 'allWeight' in weight_selector:
+            dff = df
+        elif len(weight_selector) < 1:
+            dff = df
+        else:
+            dff = df[df['weight'].isin(weight_selector)]
+
+        if 'allDisplaySize' in display_size_selector:
+            dff = df
+        elif len(display_size_selector) < 1:
+            dff = df
+        else:
+            dff = df[df['display_size'].isin(display_size_selector)]
+
+        if 'allTouchscreen' in touchscreen_selector:
+            dff = df
+        elif len(touchscreen_selector) < 1:
+            dff = df
+        else:
+            dff = df[df['Touchscreen'].isin(touchscreen_selector)]
+
 
         fig = px.scatter(df, x='star_rating', y='latest_price', color='star_rating', size='latest_price',
                          hover_data=['brand', 'model', 'ram_gb', 'processor_brand', 'processor_name', 'ram_type',
-                                     'ssd'])
+                                     'ssd', 'hdd', 'os', 'weight', 'display_size', 'Touchscreen'])
 
         return fig
     else:
         fig = px.scatter(df, x='star_rating', y='latest_price', color='star_rating', size='latest_price',
                          hover_data=['brand', 'model', 'ram_gb', 'processor_brand', 'processor_name', 'ram_type',
-                                     'ssd'])
+                                     'ssd', 'hdd', 'os', 'weight', 'display_size', 'Touchscreen'])
         return fig
 
 
